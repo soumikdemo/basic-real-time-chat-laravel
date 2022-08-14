7bff3ddc033ee3d695a9cfc2ff470270dabdf472
Soumik Datta <mailmeiamsa@gmail.com>
2022-08-15 04:55:46 +0600

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Pusher Test</title>
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
+    <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.3.0/bootbox.min.js" type="text/javascript"></script>
+
+    <style type="text/css">
+        .messages_display {
+            height: 300px;
+            overflow: auto;
+        }
+
+        .messages_display .message_item {
+            padding: 0;
+            margin: 0;
+        }
+
+        .bg-danger {
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    
+</body>
+
+</html>
