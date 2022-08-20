bb2a9fd9dfb75fda0609506ce2c10626f3b8a9e3
Soumik Datta <mailmeiamsa@gmail.com>
2022-08-21 05:20:48 +0600

    chatting -public channel -vanilla js|css -pusher

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,71 @@
 <!DOCTYPE html>
-
+<html lang="en">
 <head>
-    <title>Pusher Test</title>
-    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
-
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
-    <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.3.0/bootbox.min.js" type="text/javascript"></script>
-
-    <style type="text/css">
-        .messages_display {
-            height: 300px;
-            overflow: auto;
-        }
-
-        .messages_display .message_item {
-            padding: 0;
-            margin: 0;
-        }
-
-        .bg-danger {
-            padding: 10px;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Chat</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body>
+    <div class="app">
+        <header>
+            <h1>Let's Chat</h1>
+            <input type="text" name="username" id="username" placeholder="Please enter username" />
+        </header>
 
+        <div id="messages"></div>
 
-    
+        <form id="message_form">
+            <input type="text" name="message" id="message_input" placeholder="Write your message" />
+            <button type="submit" id="message_send">Send Message</button>
+        </form>
+    </div>
+
+    <script type="text/javascript">
+        // listening to channel event after window is ready
+        window.addEventListener('load', function() {
+            window.Echo.channel('chat').listen('.message', (e) => {
+                // handling event response
+                messages_el.innerHTML += '<div class="message"><strong>' + e.username + ':</strong> ' + e.message + '</div>';
+            });
+        });
+
+        const messages_el = document.getElementById('messages');
+        const username_input = document.getElementById('username');
+        const message_input = document.getElementById('message_input');
+        const message_form = document.getElementById('message_form');
+
+        message_form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            let has_errors = false;
+
+            if(username_input.value == '') {
+                alert('Please enter a username');
+                has_errors = true;
+            }
+
+            if(message_input.value == '') {
+                alert('Please enter a message');
+                has_errors = true;
+            }
+
+            if(has_errors) {
+                return;
+            }
+
+            const options = {
+                method: 'POST',
+                url: '/send-message',
+                data: {
+                    username: username_input.value,
+                    message: message_input.value
+                }
+            }
+
+            axios(options);
+        });
+
+    </script>
 </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Events\Message;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| npm install --save
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| laravel-echo, pusher-js
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/send-message', function (Request $request) {
+    event(new Message($request->username, $request->message));
 });
